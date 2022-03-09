65c2c11a774a1f740d98d9302239887bcffc3c22
Miller Meares <benmillermeares3@gmail.com>
2022-03-09 15:43:11 -0500

    web.config file that fixes things on refresh

---
+++ bracket-server/DBSetup/prod_playground.sql
@@ -0,0 +1,7 @@
+use bracket_app;
+select * from user;
+select * from user_token;
+
+
+select * from error_log;
+
---
+++ bracket-server/DBSetup/user_roles.sql
@@ -0,0 +1,13 @@
+CREATE TABLE role(
+roleKey VARCHAR(45) NOT NULL,
+PRIMARY KEY(roleKey)
+);
+
+INSERT INTO role(roleKey) VALUES("Administrator"), ("Developer");
+CREATE TABLE user_role(
+	_fk_user VARCHAR(45) NOT NULL,
+    _fk_role VARCHAR(45) NOT NULL,
+    PRIMARY KEY(_fk_user, _fk_role),
+	CONSTRAINT `user_foreign_key` FOREIGN KEY (`_fk_user`) REFERENCES `user` (`userID`),
+	CONSTRAINT `role_foreign_key` FOREIGN KEY (`_fk_role`) REFERENCES `role` (`roleKey`)
+);
