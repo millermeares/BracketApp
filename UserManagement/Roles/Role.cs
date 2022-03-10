2a7d1d3c6c9a54e146f5864e19c1977025fe04d0
Miller Meares <benmillermeares3@gmail.com>
2022-03-10 16:29:24 -0500

    bug causing issues on prod

--- UserManagement/Roles/Role.cs
+++ UserManagement/Roles/Role.cs
@@ -8,12 +8,11 @@ namespace UserManagement.Roles
 {
     public class Role
     {
-        public string Name { get; set; } = string.Empty;
-        //private string _name = string.Empty;
-        //public string Name
-        //{
-        //    get => _name.ToLower();
-        //    set => _name = value;
-        //}
+        private string _name = string.Empty;
+        public string Name
+        {
+            get => _name.ToLower();
+            set => _name = value;
+        }
     }
 }
